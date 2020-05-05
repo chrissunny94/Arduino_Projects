d3907c33509f1e89a1ef904bcc150d20bfb68b75
chrissunny94 <chrissunny94@gmail.com>
2020-05-05 11:24:51 +0530

    backed up Ecowash code

---
+++ Ecowash/pilot1/Arduino_Code/Arduino_Code.ino
@@ -0,0 +1,14 @@
+int water_pump1 = 13;
+int water_pump2 = 12;
+int water_pump3 = 11;
+int main_valve= 10;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Ecowash/pilot1/README.md
@@ -0,0 +1,2 @@
+# pilot1
+First version of the Eecowash machine 
